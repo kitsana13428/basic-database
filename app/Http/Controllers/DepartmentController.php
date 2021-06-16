d893c50a5c90303cefc8c11fbab9e5b0ef93506d
Kitsana Rattanapholsaen <kitsana971@gmail.com>
2021-06-16 23:36:24 +0700

    +

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Department;
+use Illuminate\Support\Facades\Auth;
 
 class DepartmentController extends Controller
 {
@@ -11,8 +13,20 @@ class DepartmentController extends Controller
     }
 
     public function store (Request $request){
+        //ตรวจสอบข้อมูล
         $request->validate([
             'department_name'=>'required|unique:departments|max:255'
-        ]);
+        ],
+        [
+            'department_name.required'=>"กรุณากรอกชื่อแผนก",
+            'department_name.max'=>"ห้ามป้อนเกิน 255 ตัวอักษร"
+        ]
+
+        );
+        //บันทึกข้อมูล
+        $department = new Department;
+        $department->department_name = $request->department_name;
+        $department->user_id = Auth::user()->id;
+        $department->save();
     }
 }
